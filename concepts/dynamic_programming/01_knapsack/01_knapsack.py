b0d5c3e6d2594bcf491de341d20455fb0605d5d5
Long Liang <lliang@Earth-C-137.local>
2019-12-02 12:49:07 -0800

    implement knapsack solutions.

---
+++ concepts/dynamic_programming/01_knapsack/01_knapsack.py
@@ -0,0 +1,28 @@
+"""
+Imagine that you have a knapsack which can carry a certain maximum amount of weight
+and you have a set of items with their own weight and a monetary value.
+You are going to to sell your items in the market but you can only carry what fits in the knapsack.
+How do you maximize the amount of money that you can earn?
+
+
+Given a list of items with values and weights, as well as a max weight,
+find the maximum value you can generate from items,
+where the sum of the weights is less than or equal to the max. eg.
+
+items = {(w:2, v:6), (w:2, v:10), (w:3, v:12)}
+max weight = 5
+knapsack(items, max weight) = 22
+"""
+
+import unittest
+
+
+class Knapsack:
+  def __init__(self):
+    pass
+
+
+
+class TestKnapsack(unittest.Testcase):
+  def test_brute_force_solution(self):
+    pass
